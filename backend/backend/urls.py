5ac2647968e9179d02f3ce8eba6764953bced6ba
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2022-11-28 11:57:25 +0530

    feat: add static url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('users.urls', namespace='users')),
     path('api/subscriptions/', include('base.urls', namespace='base')),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
